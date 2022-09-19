ad1efdf8ddab5374802cb7ae7130891f54c7965f
Masha <39933741+Marechka@users.noreply.github.com>
2022-09-18 18:31:06 -0700

    Modal component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Header from "../src/components/Layout/Header";
 import Meals from "../src/components/Meals/Meals";
+import Cart from '../src/components/Cart/Cart'
 
 function App() {
   return (
     <Fragment>
+      <Cart/>
       <Header />
       <main>
         <Meals />
